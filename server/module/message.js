b3a37895bad7a9db387f6d5a68cce3cf180b3302
zhuo <510348414@qq.com>
2018-12-18 15:31:39 +0800

    新版的博客静态

---
+++ server/module/message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+// 发布文章模板
+const post_message = new mongoose.Schema({
+  id: Number,
+  userName:String,
+  email:String,
+  content:String,
+  pic:String
+})
+
+module.exports = mongoose.model('PostMessage', post_message)
