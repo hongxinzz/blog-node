354c64524c0ba49bf68444c43805aad6ff58658c
zhuo <510348414@qq.com>
2019-01-12 17:16:42 +0800

    新版

--- server/server.js
+++ server/server.js
@@ -1,36 +1,27 @@
 const Koa = require('koa');
 const Router = require('koa-router');
-const path = require('path')
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('koa-bodyparser');
-const staticCache = require('koa-static-cache')
+const serve = require('koa-static');
 const app = new Koa();
 
+const home   = serve(path.join(__dirname)+'/dist/');
 // 创建一个主机名
-const hostname = '0.0.0.0'
+const hostname = '0.0.0.0';
 // 创建一个服务端号
-const port = 80
+const port = 80;
 // 连接数据库 本地需要启动
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 
 //使用bodyParser获取body(post方法)
-app
-  .use(serve('/dist', './dist'))
-  .use(serve('/public', './public'))
+app.use(home)
   .use(bodyParser())
 //引入数据库模型 并通过路由返回数据
-.use(require('./routers/article-router.js').routes())
-.use(require('./routers/message-router.js').routes())
-.use(require('./routers/user-router.js').routes())
+  .use(require('./routers/article-router.js').routes())
+  .use(require('./routers/message-router.js').routes())
+  .use(require('./routers/user-router.js').routes());
 
-function serve (prefix, filePath) {
-  return staticCache(path.resolve(__dirname, filePath), {
-    prefix: prefix,
-    gzip: true,
-    dynamic: true,
-    maxAge: 60 * 60 * 24 * 30
-  })
-}
 
 app.listen(port, hostname, () => {
   console.log(`server running at http://${hostname}:${port}/`)
