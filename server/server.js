952c790836065897ac7cc6de2b24538608b4f2ca
zhuo <510348414@qq.com>
2019-01-12 17:44:20 +0800

    新版

--- server/server.js
+++ server/server.js
@@ -2,21 +2,19 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const path = require('path');
 const mongoose = require('mongoose');
-const bodyParser = require('koa-bodyparser');
 const serve = require('koa-static');
 const app = new Koa();
 
 const home   = serve(path.join(__dirname)+'/dist/');
 // 创建一个主机名
-const hostname = '0.0.0.0';
+const hostname = '127.0.0.1';
 // 创建一个服务端号
-const port = 80;
+const port = 8080;
 // 连接数据库 本地需要启动
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 
 //使用bodyParser获取body(post方法)
 app.use(home)
-  .use(bodyParser())
 //引入数据库模型 并通过路由返回数据
   .use(require('./routers/article-router.js').routes())
   .use(require('./routers/message-router.js').routes())
